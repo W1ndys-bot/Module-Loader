d65ee6e025981e015ca963b521df0c0c7df22782
W1ndys <w1ndys@outlook.com>
2024-08-03 17:16:55 +0800

    更新部分代码，增加依赖

--- app/handler_message.py
+++ app/handler_message.py
@@ -7,7 +7,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# from app.scripts.anonymous_handler.main import handle_anonymous_message
+
 from scripts.test.main import handle_group_message
 
 
